68d852ce85956ddaa9a2db8e7543abb09db3b5e6
techietrent07 <augustine.onu.200506@unn.edu.ng>
2023-11-08 21:55:01 -0800

    All 0x02-Python Tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    arglen = len(sys.argv) - 1
+    if arglen == 0:
+        print("0 arguments.")
+    elif arglen == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arglen))
+    for i in range(arglen):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
